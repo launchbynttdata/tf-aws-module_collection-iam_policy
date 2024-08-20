7294f6e9d2e3fe6dc4d26a46b61d45dfd5b5837c
Bryce Lynn <bryce.lynn@nttdata.com>
2024-08-20 16:45:02 -0700

    with working tests

--- tests/testimpl/test_impl.go
+++ tests/testimpl/test_impl.go
@@ -24,6 +24,18 @@ func TestIamPolicy(t *testing.T, ctx types.TestContext) {
 		output := GetPolicy(t, iamClient, policyArn)
 		require.Equal(t, policyName, *output.Policy.PolicyName, "Expected name to be %s, got %s", policyName, *output.Policy.PolicyName)
 	})
+
+	t.Run("TestDoMultiplePoliciesExist", func(t *testing.T) {
+		ctx.EnabledOnlyForTests(t, "multiple_policies")
+
+		policyArns := terraform.OutputList(t, ctx.TerratestTerraformOptions(), "policy_arns")
+		policyNames := terraform.OutputList(t, ctx.TerratestTerraformOptions(), "policy_names")
+
+		for i, policyArn := range policyArns {
+			output := GetPolicy(t, iamClient, policyArn)
+			require.Equal(t, policyNames[i], *output.Policy.PolicyName, "Expected name to be %s, got %s", policyNames[i], *output.Policy.PolicyName)
+		}
+	})
 }
 
 func GetAWSConfig(t *testing.T) (cfg aws.Config) {
